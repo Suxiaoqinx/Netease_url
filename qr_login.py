6e7c50cf6f842164141b080ca38c8877edc006ed
苏晓晴 <37541680+Suxiaoqinx@users.noreply.github.com>
2025-05-25 20:10:07 +0800

    Update qr_login.py

--- qr_login.py
+++ qr_login.py
@@ -3,11 +3,7 @@ print("开始网易云音乐二维码登录流程...")
 cookies = qr_login()
     
 if cookies:
-    #print("\nCookie信息：")
-    #print(json.dumps(cookies, indent=2, ensure_ascii=False))
-        
-    # 可以保存cookie到文件中供后续使用
-    with open('cookie.txt', 'w') as f:
-        f.write(cookies)
+    print("\nCookie信息：")
+    print(cookies)
 else:
     print("登录失败，请重试。")
